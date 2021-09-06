7b1fbddf8eee751905a410813db0c8463c0ec88a
Grigoriy Kozheletov <misteryrus@gmail.com>
2021-09-06 19:51:18 +0300

    add swiper js for slider section

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-window.addEventListener('load', () => {});
+'use strict';
+
+window.addEventListener('load', () => {
+    {
+        const swiper = new Swiper('.swiper', {
+            // Navigation arrows
+            loop: true,
+            navigation: {
+                nextEl: '.slider-arrow-prev',
+                prevEl: '.slider-arrow-next',
+            },
+        });
+    }
+})
